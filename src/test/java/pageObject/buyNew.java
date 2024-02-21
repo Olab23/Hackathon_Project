26ea8b8f6c36172d459ec795e52cd59ff9539672
Swami <2303656@cognizant.com>
2024-02-21 12:11:44 +0530

    Added Car Insurance

---
+++ src/test/java/pageObject/buyNew.java
@@ -0,0 +1,22 @@
+
+package pageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class buyNew extends BasePage {
+	public	WebDriver driver;
+	public buyNew( WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+			}
+@FindBy(xpath= "//*[@id=\"before-tp\"]/div[2]/a")
+WebElement buynew;
+
+
+public void buynew() {
+	buynew.click();
+}
+}
---
+++ src/test/java/testCases/TC_002_Car_Insurance.java
@@ -0,0 +1,85 @@
+package testCases;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import pageObject.buyNew;
+import pageObject.HomePage;
+import pageObject.rto_city;
+import testBase.BaseClass;
+
+public class TC_002_Car_Insurance extends BaseClass  {
+
+	
+	@Test(priority=0)
+	public void verify_linkOpen ()  {
+		// TODO Auto-generated constructor stub
+		
+
+	}
+
+	@Test(priority=1)
+	public void verify_carIns_click() {
+		HomePage hm = new HomePage(driver);
+		hm.Carinsurance();
+		
+	}
+	@Test(priority=2)
+	public void verify_buyNew_click() {
+		
+		buyNew bn =  new  buyNew(driver);
+		bn.buynew();
+	}
+	
+	@Test(priority=3)
+	public void verify_city_click() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.city_click();
+	}
+	
+	@Test(priority=4)
+	public void verify_rto_click() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.rto_click();
+	}
+	@Test(priority=5)
+	public void verify_brand_click() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.carbrand_click();
+	}
+	
+	@Test(priority=6)
+	public void verify_model_click() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.carModel_click();
+	}
+	@Test(priority=7)
+	public void verify_varaint_click() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.carVariant_click();
+	}
+	
+	@Test(priority=8)
+	public void sendDetails() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.send_Details();
+	}
+	
+	@Test(priority=9)
+	public void submit_click() {
+		
+		rto_city rc = new rto_city(driver);
+		rc.print_error();
+	}
+	
+	
+	
+	
+}
+
